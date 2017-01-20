fb4c29ec3f3c9988c7004edc7e6cb8d04201847d
José Eduardo Chaves Costa <zeduardu@gmail.com>
2017-01-20 09:35:48 -0300

    #enhancement - Adding secure block - Part 03

--- routes.php
+++ routes.php
@@ -3,6 +3,26 @@
 // #Organizing Routes#
 // This application will begin to define many controllers, hence the need to want to group routes logically.
 
-//Place file routes here
+// Set of controllers that management the internal data and need authentication
+$management = $app['controllers_factory'];
+
+// *******Place file routes here***********
 require __DIR__ . '/routes/enrollment.php';
 require __DIR__ . '/routes/management.php';
+// ****************************************
+
+// Allows that any functionality be it requested before the controller is executed
+$management->before(function () {
+    global $USER, $CFG;
+
+    require_login();
+    $context = context_module::instance($USER->id);
+    if (!has_capability('local/psf:manage', $context))
+    {
+        $app = new Silex\Application;
+        return $app->redirect($CFG->wwwroot);
+    }
+});
+
+// Mounts controllers under the given route prefix.
+$app->mount('management', $management);
