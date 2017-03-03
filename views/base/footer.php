74f7d959ccf2fe02737e2e9d70b2dd5e0309bb6e
José Eduardo <zeduardu@gmail.com>
2017-03-03 02:19:03 -0300

    #enhancement - Added side blocks
    Added standard blocks (navigation and administration) in the left side of page.

--- views/base/footer.php
+++ views/base/footer.php
@@ -1,3 +1,4 @@
 <?php
+global $PAGE, $OUTPUT, $SITE;
 
-?>
+echo $OUTPUT->footer();
--- views/template-html.php
+++ views/template-html.php
@@ -1,2 +1,3 @@
 <?php include __DIR__ . '/base/header.php'; ?>
 <?php $view['slots']->output('body') ?>
+<?php include __DIR__ . '/base/footer.php'; ?>
