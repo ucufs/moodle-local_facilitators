d02010b70b2995890619721c82a5a010669118ed
Jéssica Pinto <jessica_jpinto@hotmail.com>
2017-03-23 10:33:16 -0300

    enhancement - Retificação do edital

--- views/enrollment/index-html.php
+++ views/enrollment/index-html.php
@@ -15,7 +15,8 @@
     <p style="font-size: smaller; font-weight: bold;">O sistema de inscrição é melhor visualizado utilizando o <a href="https://www.google.com.br/chrome/browser/desktop/" alt="Clique aqui para fazer o download do navegador" target="_blank">Google Chrome</a>.</p>
     <p class="text-error"><b>Importante: o candidato deve iniciar a inscrição com os documentos disponíveis para serem anexados no sistema.</b></p>
   <?php endif; ?>
-  <p><a href="<?= $edict->file ?>" target="_blank">Edital</a></p>
+  <p><a href="http://progep.ufs.br/uploads/page_attach/path/2311/Retifica__o_01_Edital_01-17.pdf" target="_blank">Retificação do Edital n° <?= $edict->edict_number ?>/<?= $edict->validity_year ?></a></p>
+  <p><a href="<?= $edict->file ?>" target="_blank">Edital n° <?= $edict->edict_number ?>/<?= $edict->validity_year ?></a></p>
 </fieldset>
 <?php }; ?>
 
