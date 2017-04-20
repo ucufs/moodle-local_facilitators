6f215a0d964760a75a03f1f4b5bf439082ba5484
Jéssica Pinto <jessicajpinto01@gmail.com>
2017-04-20 08:48:47 -0300

    Publishing final result

--- views/enrollment/index-html.php
+++ views/enrollment/index-html.php
@@ -15,8 +15,9 @@
     <p style="font-size: smaller; font-weight: bold;">O sistema de inscrição é melhor visualizado utilizando o <a href="https://www.google.com.br/chrome/browser/desktop/" alt="Clique aqui para fazer o download do navegador" target="_blank">Google Chrome</a>.</p>
     <p class="text-error"><b>Importante: o candidato deve iniciar a inscrição com os documentos disponíveis para serem anexados no sistema.</b></p>
   <?php endif; ?>
+  <p><a href="http://progep.ufs.br/uploads/page_attach/path/2410/Resultado_final_da_selecao_Edital_01_17.pdf" target="_blank">Resultado Final</a></p>
   <p><a href="http://progep.ufs.br/uploads/page_attach/path/2404/Formul_rio_de_Recurso_Edital_01_17.docx" target="_blank">Formulário para Recurso</a></p>
-  <p><a href="http://progep.ufs.br/uploads/page_attach/path/2406/Resultado_da_selecao_Edital_01_17.pdf" target="_blank">Resultado</a></p>
+  <p><a href="http://progep.ufs.br/uploads/page_attach/path/2406/Resultado_da_selecao_Edital_01_17.pdf" target="_blank">Resultado Preliminar</a></p>
   <p><a href="http://progep.ufs.br/uploads/page_attach/path/2312/Retifica__o_02_Edital_01-17.pdf" target="_blank">Retificação n° 02 do Edital n° <?= $edict->edict_number ?>/<?= $edict->validity_year ?></a></p>
   <p><a href="http://progep.ufs.br/uploads/page_attach/path/2311/Retifica__o_01_Edital_01-17.pdf" target="_blank">Retificação n° 01 do Edital n° <?= $edict->edict_number ?>/<?= $edict->validity_year ?></a></p>
   <p><a href="<?= $edict->file ?>" target="_blank">Edital n° <?= $edict->edict_number ?>/<?= $edict->validity_year ?></a></p>
